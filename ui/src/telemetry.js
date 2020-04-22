20dd33bc9669887c75ff8e09fc664031c15f6aff
Nick Iodice <niiodice@microsoft.com>
2020-04-21 23:33:50 -0500

    remove console.log from telemetry

--- ui/src/telemetry.js
+++ ui/src/telemetry.js
@@ -1,7 +1,5 @@
 import { ApplicationInsights } from '@microsoft/applicationinsights-web'
 
-console.log(process.env)
-
 function loadAppInsights() {
     const appInsights = new ApplicationInsights({ config: {
       instrumentationKey: process.env.REACT_APP_APPINSIGHTS_INSTRUMENTATIONKEY,
